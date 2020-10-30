65111f32f6f0abcde15eec4eb44efd0bbb4a2ebf
Alex Markin <alexndr86@gmail.com>
2020-10-30 21:24:58 +0300

    Update projects

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -50,11 +50,11 @@ class Header extends Component {
               </a>
             </li>
             {/* <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li> */}
-            <li>
+            {/* <li>
               <a className="smoothscroll" href="#contact">
                 Contact
               </a>
-            </li>
+            </li> */}
           </ul>
         </nav>
 
